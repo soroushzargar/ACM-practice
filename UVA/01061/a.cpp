7551b4e9885eb54c6a47eb3bda1cf5d0f36ca085
Soroush Zargar <soroushzargar@gmail.com>
2016-07-21 15:44:03 +0430

    Second Contest! Create a Graph and Perform TSP! Nice Teamwork! Tnx to MEHRAN! ;)

--- UVA/01061/a.cpp
+++ UVA/01061/a.cpp
@@ -1,150 +1,65 @@
 #include <iostream>
-#include <vector>
-#include <string>
 #include <set>
-#include <map>
+#include <vector>
 
 using namespace std;
 
-string combine(string comb , bool r){
-    string result;
-    if (comb == "AA"){
-        result = "A";
-    }if (comb == "AB" || comb == "BA"){
-        result = "AB";
-    }if (comb == "BB"){
-        result = "B";
-    }if (comb == "OO"){
-        result = "O";
-    }if (comb == "OA" || comb == "AO"){
-        result = "A";
-    }if (comb == "BO" || comb == "OB"){
-        result = "B";
-    }
-    if (r) result += "+";
-    else result += "-";
-    return result;
+int abs (int a ){
+    if (a > 0) return a;
+    return (-1)*a;
+}
+#define inf 1000000000
+int start; // initialize before function
+int n;
+vector <vector <int> > dist, memo;
+int tsp(int pos, int bitmask) { // bitmask stores the visited coordinates
+    if (bitmask == (1 << (n + 1)) - 1)
+        return dist[pos][start]; // return trip to close the loop
+    if (memo[pos][bitmask] != -1)
+        return memo[pos][bitmask];
+
+    int ans = inf;
+    for (int nxt = 0; nxt <= n; nxt++) // O(n) here
+        if (nxt != pos && !(bitmask & (1 << nxt))) // if coordinate nxt is not visited yet
+            ans = min(ans, dist[pos][nxt] + tsp(nxt, bitmask | (1 << nxt)));
+    return memo[pos][bitmask] = ans;
 }
 
-
-set <string> calc (string father , string mother){
-
-
-
-    vector <char> allel_father;
-    bool rh_father;
-    for(int i = 0 ; i < father.size() ; i++){
-        if (father[i] == 'A') allel_father.push_back('A');
-        if (father[i] == 'B') allel_father.push_back('B');
-        if (father[i] == 'O') allel_father.push_back('O');
-        if (father[i] == '-') rh_father = false;
-        if (father[i] == '+') rh_father = true;
-    }
-    if (father.size() <= 2){
-        allel_father.push_back('O');
-    }
-
-    vector <char> allel_mother;
-    bool rh_mother;
-    for(int i = 0 ; i < mother.size() ; i++){
-        if (mother[i] == 'A') allel_mother.push_back('A');
-        if (mother[i] == 'B') allel_mother.push_back('B');
-        if (mother[i] == 'O') allel_mother.push_back('O');
-        if (mother[i] == '-') rh_mother = false;
-        if (mother[i] == '+') rh_mother = true;
-    }
-    if (mother.size() <= 2){
-        allel_mother.push_back('O');
-    }
-
-    set <string> possible;
-    for (int i = 0 ; i < allel_father.size() ; i++){
-        for (int j = 0 ; j < allel_mother.size() ; j++){
-            string temp; temp += allel_father[i] ; temp += allel_mother[j];
-            possible.insert(combine(temp , rh_father));
-            possible.insert(combine(temp , rh_mother));
-            possible.insert(combine(temp , false));
-            possible.insert(combine(temp , false));
-        }
-    }
-    return possible;
-}
+// the answer is in tsp(start, 1 << start); start is the index of starting node
 
 int main (){
-    map <string , int > index;
-    map <int , string> value;
+    int x , y;
+    while(cin >> x >> y){
+        vector <pair <int , int> > nuts;
+        pair <int , int> src;
 
-    index ["O-"] = 0;
-    index ["O+"] = 1;
-    index ["A-"] = 2;
-    index ["A+"] = 3;
-    index ["B-"] = 4;
-    index ["B+"] = 5;
-    index ["AB-"] = 6;
-    index ["AB+"] = 7;
-
-    value [0] = "O-";
-    value [1] = "O+";
-    value [2] = "A-";
-    value [3] = "A+";
-    value [4] = "B-";
-    value [5] = "B+";
-    value [6] = "AB-";
-    value [7] = "AB+";
-
-    vector <vector <set <string> > > possibities(8 , vector <set <string> > (8 , set <string> ()));
-
-    for (int i = 0 ; i < 8 ; i++){
-        for (int j = 0 ; j < 8 ; j++){
-            possibities[i][j] = calc(value[i],value[j]);
-        }
-    }
-
-
-    string f , m , c;
-    int case_no = 1;
-    while (cin >> f >> m >> c){
-
-        if (f == "E") return 0;
-        set <string> result;
-        if (c == "?"){
-            result = possibities[index[f]][index[m]];
-        }
-        if (f == "?"){
-            for (int i = 0 ; i < 8 ; i++){
-
-                if (possibities[i][index[m]].find(c) != possibities[i][index[m]].end()){
-                    result.insert(value[i]);
+        for (int i = 0 ; i < x; i++){
+            for (int j = 0 ; j < y ; j++){
+                char tmp; cin >> tmp;
+                if (tmp == 'L'){
+                    src = make_pair(i, j);
+                }
+                else if (tmp == '#'){
+                    nuts.push_back(make_pair(i, j));
                 }
             }
         }
-        if (m == "?"){
-            for (int j = 0 ; j < 8 ; j++){
-
-                if (possibities[index[f]][j].find(c) != possibities[index[f]][j].end()){
-                    result.insert(value[j]);
-                }
+        nuts.push_back(src);
+        dist = vector <vector <int> > (nuts.size() , vector <int> (nuts.size() , 0));
+        for (int i = 0 ; i < nuts.size() ; i++){
+            for (int j = 0 ; j < nuts.size() ; j++){
+                dist[i][j] = abs(nuts[i].first - nuts[j].first) < abs(nuts[i].second - nuts[j].second) ? abs(nuts[i].first - nuts[j].first) : abs(nuts[i].second - nuts[j].second);
+                dist[i][j] += abs(abs(nuts[i].first - nuts[j].first) - abs(nuts[i].second - nuts[j].second));
             }
         }
-        string toshow;
-        if (result.size() == 0){
-            toshow = "IMPOSSIBLE";
-        } else if (result.size() == 1){
-            toshow = *result.begin();
-        } else {
-            toshow = "{";
-            int i = 0;
-            for (set <string> :: iterator it = result.begin() ; it != result.end() ; it++ ,i++){
-                if (i != 0) toshow += ", ";
-                toshow += *it;
-                if (i == result.size() - 1) toshow += "}";
-
-            }
-        }
-        cout << "Case "  << case_no << ": "; case_no++;
-        if (f == "?") cout << toshow <<  " " << m <<  " " << c << endl;
-        if (m == "?") cout << f <<  " " << toshow <<  " " << c << endl;
-        if (c == "?") cout << f <<  " " << m <<  " " << toshow << endl;
+        memo.resize(nuts.size() + 5, vector<int>((1 << (nuts.size() + 4)), -1));
+        vector <vector <int> > &ddd = dist;
+        n = nuts.size() - 1;
+        start = nuts.size() - 1;
+        int mask = (1  << start);
+        cout << tsp(start, mask) << endl;
+        memo.clear();
+        dist.clear();
     }
     return 0;
 }
