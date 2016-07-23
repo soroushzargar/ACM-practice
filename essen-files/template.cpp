13f069f8dba38ba8d1c98930264fe8c30687ad7e
Soroush Zargar <soroushzargar@gmail.com>
2016-07-23 18:27:52 +0430

    Template just same as alireza omidi :))

---
+++ essen-files/template.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <string>
+#include <iomanip>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef pair<int, ii> iii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<iii> viii;
+typedef vector<vii> vvii;
+typedef set<int> si;
+typedef vector<si> vsi;
+typedef pair<double, double> dd;
+
+const int inf = 1e9;
+
+int main() {
+	ios::sync_with_stdio(false);
+
+
+
+	return 0;
+}
